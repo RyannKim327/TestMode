af98c58f3de286dcad5a54ac0c4fded3f836412b
RyannKim327 <rksesgundo123@gmail.com>
2025-02-09 15:23:43 +0800

    02-09-25 15:23

--- user/bible.js
+++ user/bible.js
@@ -1,4 +1,15 @@
 const axios = require("axios");
 const bible = require("biblegateway-scrape");
 
-module.exports = async (api, event, result) => {};
+module.exports = async (api, event, result) => {
+  const data = await bible.verse(
+    result[1],
+    bible.version.ENG_KING_JAMES_VERSION
+  );
+  // console.log(data);
+  api.sendMessage(
+    `Bible Verse:\nVerse: ${data[0].book}\nPassage: ${data[0].verse}`,
+    event.threadID,
+    (err, msg) => {}
+  );
+};
